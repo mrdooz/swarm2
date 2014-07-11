52c0bbe0feb0d4e4c45478eaa11f2acf13f7163c
Magnus Österlind <magnus.osterlind@gmail.com>
2014-07-10 18:46:54 -0700

    Playing with some channel stuff..

--- client/websocket.js
+++ client/websocket.js
@@ -1,66 +1,157 @@
 var ProtoBuf = dcodeIO.ProtoBuf;
 var ByteBuffer = ProtoBuf.ByteBuffer;
 var builder;
-var wsUri = "ws://127.0.0.1:8080/";
-var output;
 
-var p = "package connection; message ConnectionResponse { optional uint32 connectionId = 1; }";
+var g_clientId;
+var g_connectionManager;
 
-
-function init() {
-    console.log("test");
-    output = document.getElementById("output");
-//    builder = ProtoBuf.loadProtoFile("http://127.0.0.1:8000/protocol/connection.proto");
-    builder = ProtoBuf.loadProto(p);
-    testWebSocket();
+function fnv32a(str)
+{
+    var FNV1_32A_INIT = 0x811c9dc5;
+    var hval = FNV1_32A_INIT;
+    for (var i = 0; i < str.length; ++i)
+    {
+        hval ^= str.charCodeAt(i);
+        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+    }
+    return hval >>> 0;
 }
 
-function testWebSocket() {
-    websocket = new WebSocket(wsUri);
-    websocket.onopen = function (evt) {
-        onOpen(evt)
+// concatenate a list of ArrayBuffers into a single buffer
+function concatBuffers(bufs) {
+    var len = 0;
+    _.each(bufs, function(e, i, l) { len += e.length; });
+
+    var tmp = new Uint8Array(len);
+    var ofs = 0;
+    _.each(bufs, function(e, i, l) {
+        tmp.set(new Uint8Array(e), ofs);
+        ofs += e.length;
+    });
+
+    return tmp.buffer;
+}
+
+function ConnectionManager()
+{
+    this.websocket = null;
+    this.nextToken = 0;
+    this.RpcHeader = builder.build("swarm.Header");
+    this.tokenToCallback = {}
+
+    ConnectionManager.prototype.connect = function(url)
+    {
+        this.websocket = new WebSocket(url);
+        this.websocket.binaryType = 'arraybuffer';
+
+        var that = this;
+
+        this.websocket.onopen       = function(e) { that.onConnect(e); }
+        this.websocket.onclose      = function(e) { that.onClose(e); }
+        this.websocket.onmessage    = function(e) { that.onMessage(e); }
+        this.websocket.onerror      = function(e) { that.onError(e); }
     };
-    websocket.onclose = function (evt) {
-        onClose(evt)
+
+    ConnectionManager.prototype.send = function(a)
+    {
+        this.websocket.send(a.toArrayBuffer());
     };
-    websocket.onmessage = function (evt) {
-        onMessage(evt)
-    };
-    websocket.onerror = function (evt) {
-        onError(evt)
-    };
+
+    ConnectionManager.prototype.sendProtoRequest = function(methodName, attr, cb)
+    {
+        var bb = new ByteBuffer();
+        // build the header
+        var name = 'swarm.' + methodName + 'Request';
+        var hash = fnv32a(name);
+        var token = this.nextToken;
+        this.nextToken = this.nextToken + 1;
+
+        var header = new this.RpcHeader({ 
+            'method_hash' : hash,
+            'token' : token,
+            'is_response' : false});
+        var headerBuf = header.toArrayBuffer();
+
+        var Msg = builder.build(name);
+        var body = new Msg(attr);
+        var bodyBuf = body.toArrayBuffer();
+
+        var buf = new ByteBuffer(2 + headerBuf.byteLength + bodyBuf.byteLength);
+
+        // payload is: header_size + header + body
+        buf.writeInt16(headerBuf.byteLength);
+        buf.append(headerBuf);
+        buf.append(bodyBuf);
+
+        buf.reset()
+        this.websocket.send(buf.toArrayBuffer());
+
+        if (cb) {
+            this.tokenToCallback[token] = { 'methodName' : methodName, 'callback' : cb };
+        }
+    }
+
+    ConnectionManager.prototype.close = function()
+    {
+        this.websocket = null;
+    }
+
+    ConnectionManager.prototype.onConnect = function(evt)
+    {
+        var status = document.getElementById("connection-status");
+        status.innerHTML = 'Connected';
+        this.sendProtoRequest('Connection', {});
+    }
+
+    ConnectionManager.prototype.onClose = function(evt)
+    {
+    }
+
+    ConnectionManager.prototype.onError = function(evt)
+    {
+        console.log(evt);
+    }
+
+    ConnectionManager.prototype.onMessage = function(evt)
+    {
+        var data = evt.data;
+        // decode the header
+        var header = this.RpcHeader.decode(data);
+
+        if (header.is_response()) {
+
+            // lookup callback, deserialize and dispatch
+            var obj = this.tokenToCallback(header.token());
+            if (obj) {
+                // create a builder for the response object
+                var name = 'swarm.' + obj.methodName + 'Response';
+                var Msg = builder.build(name);
+                var proto = Msg.decode(data);
+                obj.callback(proto);
+            }
+            else
+            {
+                console.log('no callback found for token: ', header.token());
+            }
+        }
+
+    }
 }
 
-function onOpen(evt) {
-    writeToScreen("CONNECTED");
-    var Response = builder.build("connection.ConnectionResponse");
-    var response = new Response();
-    response.connectionId = 42;
-    doSend(response.toBuffer());
-    console.log(response);
+function createProtobuf(msg, attr)
+{
+    var Msg = builder.build(msg);
+    return new Msg(attr);
 }
 
-function onClose(evt) {
-    writeToScreen("DISCONNECTED");
+function createConnectionManager()
+{
+    g_connectionManager = new ConnectionManager();
+
 }
 
-function onMessage(evt) {
-    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
-    websocket.close();
-}
-
-function onError(evt) {
-    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-}
-
-function doSend(message) {
-    writeToScreen("SENT: " + message);
-    websocket.send(message);
-}
-
-function writeToScreen(message) {
-    var pre = document.createElement("p");
-    pre.style.wordWrap = "break-word";
-    pre.innerHTML = message;
-    output.appendChild(pre);
+function init(url) {
+    builder = ProtoBuf.loadProtoFile("http://127.0.0.1:8000/protocol/swarm.proto");
+    g_connectionManager = new ConnectionManager();
+    g_connectionManager.connect(url);
 }
--- server/server.go
+++ server/server.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"code.google.com/p/goprotobuf/proto"
+	"encoding/binary"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/mrdooz/swarm2/protocol"
+	"hash/fnv"
 	"log"
 	"net/http"
 )
 
 var (
-	conn websocket.Conn
+	conn            websocket.Conn
+	clientId        uint32
+	nextToken       uint32
+	requestHandlers map[uint32]chan []byte = make(map[uint32]chan []byte)
 )
 
 func checkOrigin(r *http.Request) bool {
 	return true
 }
 
+func ConnectRequestHandler(c chan []byte) {
+	for {
+		buf := <-c
+		request := swarm.ConnectionRequest{}
+		if err := proto.Unmarshal(buf, &request); err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("got stuff :) %d\n", request.GetDummy())
+
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 
+	// upgrade the request to websocket
 	upgrader := &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
 
 	upgrader.CheckOrigin = checkOrigin
@@ -28,25 +47,71 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var conResp = connection.ConnectionResponse{}
-
 	for {
-		messageType, p, err := conn.ReadMessage()
+		// wait for packet
+		_, buf, err := conn.ReadMessage()
 		if err != nil {
+			fmt.Print(err)
 			return
 		}
 
-		proto.Unmarshal(p, &conResp)
-		fmt.Println(conResp.GetConnectionId())
+		log.Printf("recv %d bytes\n", len(buf))
 
-		if err = conn.WriteMessage(messageType, p); err != nil {
+		// create reader, and parse header size
+		reader := bytes.NewReader(buf)
+		var headerSize int16
+		if err = binary.Read(reader, binary.BigEndian, &headerSize); err != nil {
+			fmt.Print(err)
 			return
 		}
+
+		// parse header
+		header := swarm.Header{}
+		if err = proto.Unmarshal(buf[2:2+headerSize], &header); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if !header.GetIsResponse() {
+			// look up a channel for the hash
+			ch := requestHandlers[header.GetMethodHash()]
+			ch <- buf[2+headerSize:]
+		}
+
+		fmt.Printf("header size: %d, hash: %x, token: %d\n",
+			headerSize, header.GetMethodHash(), header.GetToken())
+
+		// find the reader for the response
+
+		// parse body
+		/*
+				body := swarm.ConnectionResponse{}
+				if err = proto.Unmarshal(buf[2+headerSize:], &body); err != nil {
+					fmt.Println(err)
+					return
+				}
+			fmt.Printf("connection id: %d\n", body.GetConnectionId())
+		*/
+
 	}
+}
+
+func hash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
 }
 
 func main() {
 	log.Println("** server started")
+	c := make(chan []byte)
+	requestHandlers[hash("swarm.ConnectionRequest")] = c
+	go ConnectRequestHandler(c)
+
+	h := fnv.New32a()
+	h.Write([]byte("magnus"))
+	log.Print(h.Sum32())
+	//	log.Print(hash.New32a().)
 
 	http.HandleFunc("/", handler)
 	//	http.HandleFunc("/ws", serveWs)
